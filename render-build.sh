0ba3089d15a40b00937609512aa3f9c200321308
Harlogs <stepnest02@gmail.com>
2025-10-09 00:16:49 +0530

    added22

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-# exit on errorset -o errexit
+# exit on errors
+set -o errexit
 
 npm install
 npm install puppeteer
 # npm run build # uncomment if required
 
 # Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-  echo "...Copying Puppeteer Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-else 
-  echo "...Storing Puppeteer Cache in Build Cache" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+if [[ "$PUPPETEER_CACHE_DIR" != "$XDG_CACHE_HOME/puppeteer" ]]; then
+  if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+    echo "...Copying Puppeteer Cache from Build Cache" 
+    cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  else 
+    echo "...Storing Puppeteer Cache in Build Cache" 
+    cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+  fi
+else
+  echo "Puppeteer cache paths are the same, skipping copy"
 fi
